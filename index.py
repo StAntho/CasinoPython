2bf06d06279effe34663c1d08a6437c529d43403
Jeremy-BD <jeremy.boisnard16@gmail.com>
2022-10-31 17:32:08 +0100

    upgrade script

--- index.py
+++ index.py
@@ -1,30 +1,49 @@
 from random import randrange
 
-instruction_txt = """\nJe viens de penser à un nombre entre 1 et 10. Devinez lequel ?
-Att : vous avez le droit à trois essais !
-Si vous devinez mon nombre dès le premier coup, vous gagnez le double de votre mise !
-Si vous le devinez au 2è coup, vous gagnez exactement votre mise !
-Si vous le devinez au 3è coup, vous gagnez la moitiè votre mise !
-Si vous ne le devinez pas au 3è coup, vous perdez votre mise et vous avez le droit :
-\t- de retenter votre chance avec l'argent qu'il vous reste pour reconquérir le level perdu.
-\t- de quitter le jeu.
-Dès que vous devinez mon nombre : vous avez le droit de quitter le jeu et de partir avec vos gains OU de continuer le jeu en passant au level supérieur."""
 
-print("Je suis Python. Quel est votre pseudo ?")
-name_user = input('')
+# Initialisation de la mise de l'utilisateur
+def set_mise(level = 1) :
+	sold = 10 # TODO: Il faut récupérer le solde de l'utilisateur
+	if level != 1 or sold != 10 :
+		print("\nEntrez votre mise :")
+		while True :
+			try :
+				mise = int(input(''))
+				if mise > sold :
+					print("Erreur, votre mise est plus elevé que votre solde.")
+					print("Entrer une mise inférieure ou égale à " + sold + " € :")
+					continue
+				return mise
+			except ValueError :
+				print("Le montant saisi n'est pas valide.")
+				continue
+	else :
+		while True :
+			try :
+				mise = int(input(''))
+				if mise > 0 and mise < 11 :
+					return mise
+				else :
+					print("Le montant saisi n'est pas valide. Entrer SVP un montant entre 1 et 10 € :")
+			except ValueError :
+				print("Le montat saisi n'est pas valide. Entrer SVP un montant entre 1 et 10 € :")
 
+
+# Inclusion du jeu
 def game(level, mise) :
-	level = int(level)
-	mise = int(mise)
 	while True :
 		match level :
 			case 1 :
 				nb_ordi = randrange(0, 11, 1)
 				nb_coup_max = 3
 			case 2 :
+				print("\nRappelez vous, le principe est le même sauf que mon nombre est maintenant entre 1 et 20 et vous avez le droit à 5 essais !")
+				mise = set_mise(level)
 				nb_ordi = randrange(0, 21, 1)
 				nb_coup_max = 5
 			case 3 :
+				print("\nRappelez vous, le principe est le même sauf que mon nombre est maintenant entre 1 et 30 et vous avez le droit à 7 essais !")
+				mise = set_mise(level)
 				nb_ordi = randrange(0, 31, 1)
 				nb_coup_max = 7
 
@@ -32,10 +51,10 @@ def game(level, mise) :
 		nb_coup = 0
 		print("Mon choix : " + str(nb_ordi))
 		while nb_ordi != nb_user :
+			# TODO: Timer de 10 secondes
 			print("Alors mon nombre est ?")
-			nb_user = input('')
 			try :
-				nb_user = int(nb_user)
+				nb_user = int(input(''))
 				nb_coup += 1
 				if nb_user > nb_ordi :
 					print("Votre nbre est trop grand !")
@@ -43,27 +62,36 @@ def game(level, mise) :
 					print("Votre nbre est trop petit !")
 				elif nb_user == nb_ordi :
 					if nb_coup == 1 :
-						gain = mise *2
+						gain = mise * 2
 					elif nb_coup == 2 :
 						gain = mise
 					elif nb_coup == 3 :
 						gain = mise / 2
-					level += 1
-					print("Bingo " + name_user + ", vous avez gagné en " + str(nb_coup) + "coup(s) et vous avec emporté " + str(gain) + " € !")
+					print("Bingo " + name_user + ", vous avez gagné en " + str(nb_coup) + " coup(s) et vous avec emporté " + str(gain) + " € !")
+					# TODO: Il faut enregistrer en base les stats (nb_coup, gain, mise)
 					return True
 
 				if nb_coup == nb_coup_max :
-					if level != 1 :
-						level -= 1
 					print("Vous avez perdu ! Mon nombre est " + str(nb_ordi) + " !")
+					# TODO: Il faut enregistrer en base les stats (nb_coup, gain, mise)
 					return False
 				else :
 					print("Il vous reste " + str(nb_coup_max - nb_coup) + " chance(s) !")
 					continue
-			except :
+			except ValueError :
 				print("Je ne comprends pas ! Entrer SVP un nombre entre 1 et 10 :")
 				continue
 
+
+# Fonction affichant toutes les stats de l'utilisateur
+def show_stat(level) :
+	# TODO: Il faut récupérer toutes les stats
+	print("Les statistiques du level " + str(level) + " sont les suivantes :")
+	print("\n\t")
+	return
+
+
+# Fonction pour savoir si l'utilisateur souhaite quitter le jeu
 def leave() :
 	print("\n Souhaitez-vous continuer la partie (O/N) ?")
 	while True :
@@ -74,6 +102,21 @@ def leave() :
 			return False
 		else :
 			print("Je ne comprends pas votre réponse. Souhaitez-vous continuer la partie (O/N) ?")
+
+
+instruction_txt = """\nJe viens de penser à un nombre entre 1 et 10. Devinez lequel ?
+Att : vous avez le droit à trois essais !
+Si vous devinez mon nombre dès le premier coup, vous gagnez le double de votre mise !
+Si vous le devinez au 2è coup, vous gagnez exactement votre mise !
+Si vous le devinez au 3è coup, vous gagnez la moitiè votre mise !
+Si vous ne le devinez pas au 3è coup, vous perdez votre mise et vous avez le droit :
+\t- de retenter votre chance avec l'argent qu'il vous reste pour reconquérir le level perdu.
+\t- de quitter le jeu.
+Dès que vous devinez mon nombre : vous avez le droit de quitter le jeu et de partir avec vos gains OU de continuer le jeu en passant au level supérieur."""
+
+
+print("Je suis Python. Quel est votre pseudo ?")
+name_user = input('')
 
 while name_user :
 	if name_user :
@@ -109,25 +152,28 @@ while name_user :
 
 	# Insertion de la mise
 	print("\nLe jeu commence, entrez votre mise :")
-	while True :
-		mise = input('')
-		try :
-			mise = int(mise)
-			if mise > 0 and mise < 11 :
-				break
-			else :
-				print("Le montant saisi n'est pas valide. Entrer SVP un montant entre 1 et 10 € :")
-		except :
-			print("Le montant saisi n'est pas valide. Entrer SVP un montant entre 1 et 10 € :")
+	mise = set_mise(level)
 
 	# Début du jeu
 	print("\nLevel " + str(level))
 	while True :
-		if game(str(level), str(mise)) :
-			print("Gagné")
+		game_ret = game(level, mise)
+		if game_ret :
+			if level != 3 :
+				show_stat(level)
+				level += 1
+			else :
+				show_stat(level)
+				break
+		else :
+			if level != 1 :
+				level -= 1
 
 		if leave() :
-			print("\nSuper ! Vous passez au Level " + str(level) + ".")
+			if game_ret :
+				print("\nSuper ! Vous passez au Level " + str(level) + ".")
+			else :
+				print("\nVous passez au Level " + str(level) + ".")
 			continue
 		print("\nAu revoir ! Vous finissez la partie avec " + str(mise) + " €.")
 		break
