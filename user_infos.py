a193a4e27d912efd0c4bc9cea273a8bb284b5c4d
Evrard97 <Aka-evrard.aka@outlook.com>
2022-11-02 13:50:21 +0100

    user infos

--- user_infos.py
+++ user_infos.py
@@ -1,7 +1,7 @@
 import db_connection
 
 
-def user_informations(pseudo):
+def user_informations(pseudo,user_id):
     user = db_connection.select_user_by_psuedo(pseudo)
     for row in user:
         print("--------------------------------------------")
@@ -14,7 +14,20 @@ def user_informations(pseudo):
         print("--------------------------------------------")
         print("--------------------------------------------")
 
+    user_stats = db_connection.get_all_stat_by_user_id(10)
+    for row in user_stats:
+        print("------------Statistiques-------------------")
+        print("---- Level max: ", row[0])
+        print("---- Gain min: ", row[1])
+        print("---- Gain moy: ", row[2])
+        print("---- Gain max: ", row[3])
+        print("---- Mise min: ", row[4])
+        print("---- Mise moy: ", row[5])
+        print("---- Mise max: ", row[6])
+        print("---- Nb coup moy: ", row[7])
+        print("--------------------------------------------")
+        print("--------------------------------------------")
 
 if __name__ == '__main__':
-    user_informations('azerty')
+    user_informations('azerty',10)
    
