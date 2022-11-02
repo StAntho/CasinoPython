ba5a6d7b9d396659255a2d35fdf3c6a10a34b9b6
Evrard97 <Aka-evrard.aka@outlook.com>
2022-11-02 11:59:08 +0100

    some sql query

--- user_infos.py
+++ user_infos.py
@@ -4,11 +4,16 @@ import db_connection
 def user_informations(pseudo):
     user = db_connection.select_user_by_psuedo(pseudo)
     for row in user:
-        print("id: ", row[0])
-        print("Pseudo: ", row[1])
-        print("Sold: ", row[2])
-        print("Current level: ", row[3])
-        print("First connection: ", row[4])
+        print("--------------------------------------------")
+        print("------------Information---------------------")
+        print("---- id: ", row[0])
+        print("---- Pseudo: ", row[1])
+        print("---- Sold: ", row[2])
+        print("---- Current level: ", row[3])
+        print("---- First connection: ", row[4])
+        print("--------------------------------------------")
+        print("--------------------------------------------")
+
 
 if __name__ == '__main__':
     user_informations('testData4')
