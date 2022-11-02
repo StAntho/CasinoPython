df8ef577f7fc491a975841bf984d99ea9ab1c674
Evrard97 <Aka-evrard.aka@outlook.com>
2022-11-02 11:38:58 +0100

    some sql query

--- user_infos.py
+++ user_infos.py
@@ -3,8 +3,13 @@ import db_connection
 
 def user_informations(pseudo):
     user = db_connection.select_user_by_psuedo(pseudo)
-    return user
+    for row in user:
+        print("id: ", row[0])
+        print("Pseudo: ", row[1])
+        print("Sold: ", row[2])
+        print("Current level: ", row[3])
+        print("First connection: ", row[4])
 
 if __name__ == '__main__':
-    user = user_informations('')
-    print(user)
+    user_informations('testData4')
+   
