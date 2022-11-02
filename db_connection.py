84bddd23153dc6003f7c48f357f0498009cfac02
Evrard97 <Aka-evrard.aka@outlook.com>
2022-11-02 10:25:30 +0100

    some sql query

--- db_connection.py
+++ db_connection.py
@@ -19,11 +19,12 @@ def connectDB():
     except Error as e:
         print("Failed to connect: {}".format(e))
 
-def insertData(pseudo,sold,datetime):
+
+def insert_data(pseudo,sold,cur_level,datetime):
     db = connectDB()
-    mySql_insert_query = """INSERT INTO User (pseudo, sold, first_connection) VALUES (%s, %s,%s) """
+    mySql_insert_query = """INSERT INTO User (pseudo, sold, first_connection) VALUES (%s, %s,%s,%s) """
     cursor = db.cursor()
-    record = (pseudo, sold, datetime)
+    record = (pseudo, sold,cur_level ,datetime)
     cursor.execute(mySql_insert_query, record)
     db.commit()
     print(cursor.rowcount, "Record inserted successfully into User table")
@@ -53,14 +54,41 @@ def delete_user(username):
     db.commit()
     print(cursor.rowcount, "Record deleted successfully from User table")
 
-# Path: main.py
+def insert_level(user_id,level,mise,gain,nb_coup):
+    db = connectDB()
+    mySql_insert_query = """INSERT INTO Level (level) VALUES (%s,%s,%s,%s,%s) """
+    cursor = db.cursor()
+    record = (user_id,level,mise,gain,nb_coup)
+    cursor.execute(mySql_insert_query, record)
+    db.commit()
+    print(cursor.rowcount, "Record inserted successfully into Level table")
+
+
+def select_user_level(user_name):
+    db = connectDB()
+    mySql_select_query = """SELECT current_level FROM User WHERE pseudo = %s"""
+    cursor = db.cursor()
+    cursor.execute(mySql_select_query, (user_name,))
+    records = cursor.fetchall()
+    return records
+
+def update_user_sold_level(sold,current_level):
+    db = connectDB()
+    mySql_update_query = """UPDATE User SET sold = %s, current_level = %s WHERE pseudo = %s"""
+    cursor = db.cursor()
+    cursor.execute(mySql_update_query, (sold,current_level))
+    db.commit()
+    print(cursor.rowcount, "Record updated successfully into User table")
+
+
 if __name__ == "__main__":
     # recods = select_user()
     # print(recods)
     # record = select_user_by_psuedo("testData2")
     # print(record)
-    date = datetime.now()
-    # datenow = date.strftime("%d/%m/%Y %H:%M:%S")
-    insertData('testData9',45,date)
-    recods = select_user()
-    print(recods)
+    # date = datetime.now()
+    # # datenow = date.strftime("%d/%m/%Y %H:%M:%S")
+    # insertData('testData9',45,date)
+    # recods = select_user()
+    record = select_user_level("testData2")
+    print(record)
